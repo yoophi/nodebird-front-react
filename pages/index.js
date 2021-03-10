9666056090d22e5d2f77eed9a799d1d55f5bfb52
Pyunghyuk Yoo <yoophi@gmail.com>
2021-03-10 22:18:51 +0900

    ✨ UPDATE: index page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
 import AppLayout from "../components/AppLayout";
-import Head from "next/head";
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+
+const dummy = {
+  isLoggedIn: true,
+  imagePaths: [],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "sample",
+      },
+      content: "First post",
+      Images: [
+        {
+          src: "http://via.placeholder.com/450x600",
+        },
+        {
+          src: "http://via.placeholder.com/450x600/FFFF00",
+        },
+        {
+          src: "http://via.placeholder.com/450x600/FF00FF",
+        },
+      ],
+    },
+  ],
+};
 
 const Home = () => (
   <AppLayout>
-    <Head>
-      <title>NodeBird</title>
-    </Head>
-    <div>Hello, Next!</div>
+    {dummy.isLoggedIn && <PostForm />}
+    {dummy.mainPosts.map((c) => {
+      return <PostCard key={c.id} post={c} />;
+    })}
   </AppLayout>
 );
 
