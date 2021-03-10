e059d6d25485b4131e50808b5ebaeaebff9baaed
Pyunghyuk Yoo <yoophi@gmail.com>
2021-03-10 22:28:17 +0900

    ✨ UPDATE: PostForm

--- components/PostForm.js
+++ components/PostForm.js
@@ -1,7 +1,50 @@
-import React from "react";
+import { Button, Form, Input } from "antd";
+import React, { useCallback, useRef } from "react";
+
+const dummy = {
+  isLoggedIn: true,
+  imagePaths: [],
+  mainPosts: [
+    {
+      User: {
+        id: 1,
+        nickname: "sample",
+      },
+      content: "First post",
+      img: "http://via.placeholder.com/450x600",
+    },
+  ],
+};
 
 const PostForm = () => {
-  return <div>PostForm</div>;
+  const imageInput = useRef();
+  const onClickImageUpload = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
+  return (
+    <Form style={{ margin: "10px 0 20px" }} encType="multipart/form-data">
+      <Input.TextArea maxLength={140} placeholder="What's up?"></Input.TextArea>
+      <div>
+        <input type="file" multiple hidden ref={imageInput} />
+        <Button onClick={onClickImageUpload}>Image Upload</Button>
+        <Button type="primary" style={{ float: "right" }} htmlType="submit">
+          Tweet
+        </Button>
+      </div>
+      <div>
+        {dummy.imagePaths.map((v) => {
+          return (
+            <div key={v} style={{ display: "inline-block" }}>
+              <img src={`${v}`} style={{ width: "200px" }} alt={v} />
+              <div>
+                <Button>Remove</Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </Form>
+  );
 };
 
 export default PostForm;
