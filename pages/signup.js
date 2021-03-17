deddd9d839a53440eaa13abff1f35dc8ecb380ba
Pyunghyuk Yoo <yoophi@gmail.com>
2021-03-17 22:26:14 +0900

    ✨ IMPROVE: update signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,11 @@
 import { Button, Checkbox, Form, Input } from "antd";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Router from "next/router";
 
 import AppLayout from "../components/AppLayout";
 import Head from "next/head";
+import { signUpAction } from "../reducers/user";
 import useInput from "../hooks/useInput";
 
 const Signup = () => {
@@ -14,100 +17,107 @@ const Signup = () => {
   const [id, onChangeId] = useInput("");
   const [nick, onChangeNick] = useInput("");
   const [password, onChangePassword] = useInput("");
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
 
-  const onSubmit = () => {
+  useEffect(() => {
+    if (user) {
+      Router.push("/");
+    }
+  }, [user && user.id]);
+
+  const onSubmit = useCallback(() => {
     if (password !== passwordCheck) {
-      setPasswordError(true);
-      return;
+      return setPasswordError(true);
     }
     if (!term) {
-      setTermError(true);
-      return;
+      return setTermError(true);
     }
-    console.log({
-      id,
-      nick,
-      password,
-      passwordCheck,
-      term,
-    });
-  };
+    dispatch(
+      signUpAction({
+        id,
+        password,
+        nick,
+      })
+    );
+  }, [password, passwordCheck, term]);
 
-  const onChangePasswordCheck = (e) => {
-    setPasswordError(e.target.value !== password);
-    setPasswordCheck(e.target.value);
-  };
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordError(e.target.value !== password);
+      setPasswordCheck(e.target.value);
+    },
+    [password]
+  );
 
-  const onChangeTerm = (e) => {
+  const onChangeTerm = useCallback((e) => {
     setTermError(false);
     setTerm(e.target.checked);
-  };
+  }, []);
 
   return (
-    <>
+    <AppLayout>
       <Head>
         <title>NodeBird</title>
       </Head>
-      <AppLayout>
-        <Form onFinish={onSubmit} style={{ padding: 10 }}>
-          <div>
-            <label htmlFor="user-id">Username</label>
-            <br />
-            <Input name="user-id" value={id} required onChange={onChangeId} />
-          </div>
-          <div>
-            <label htmlFor="user-nick">Nickname</label>
-            <br />
-            <Input
-              name="user-nick"
-              value={nick}
-              required
-              onChange={onChangeNick}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password">Password</label>
-            <br />
-            <Input
-              name="user-password"
-              type="password"
-              value={password}
-              required
-              onChange={onChangePassword}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password-check">Password check</label>
-            <br />
-            <Input
-              name="user-password-check"
-              type="password"
-              value={passwordCheck}
-              required
-              onChange={onChangePasswordCheck}
-            />
-            {passwordError && (
-              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
-            )}
-          </div>
-          <div>
-            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
-              I agree to the terms and conditions.
-            </Checkbox>
-            {termError && (
-              <div style={{ color: "red" }}>
-                You must agree to the terms and conditions.
-              </div>
-            )}
-          </div>
-          <div style={{ marginTop: 10 }}>
-            <Button type="primary" htmlType="submit">
-              Join
-            </Button>
-          </div>
-        </Form>
-      </AppLayout>
-    </>
+      <Form onFinish={onSubmit} style={{ padding: 10 }}>
+        <div>
+          <label htmlFor="user-id">Username</label>
+          <br />
+          <Input name="user-id" value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor="user-nick">Nickname</label>
+          <br />
+          <Input
+            name="user-nick"
+            value={nick}
+            required
+            onChange={onChangeNick}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">Password</label>
+          <br />
+          <Input
+            name="user-password"
+            type="password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password-check">Password check</label>
+          <br />
+          <Input
+            name="user-password-check"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && (
+            <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+          )}
+        </div>
+        <div>
+          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+            I agree to the terms and conditions.
+          </Checkbox>
+          {termError && (
+            <div style={{ color: "red" }}>
+              You must agree to the terms and conditions.
+            </div>
+          )}
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <Button type="primary" htmlType="submit">
+            Join
+          </Button>
+        </div>
+      </Form>
+    </AppLayout>
   );
 };
 
