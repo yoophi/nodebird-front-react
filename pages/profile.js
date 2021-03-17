31e0b98b3207748356d03d5eb0d388a7ec88f7f6
Pyunghyuk Yoo <yoophi@gmail.com>
2021-03-17 22:38:33 +0900

    ✨ IMPROVE: update profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,14 +1,43 @@
-import AppLayout from "../components/AppLayout";
-import Head from "next/head";
-import React from "react";
+import React, { useEffect } from "react";
 
-const Profile = () => (
-  <AppLayout>
-    <Head>
-      <title>NodeBird</title>
-    </Head>
-    <div>My Profile</div>
-  </AppLayout>
-);
+import AppLayout from "../components/AppLayout";
+import FollowList from "../components/FollowList";
+import Head from "next/head";
+import NicknameEditForm from "../components/NicknameEditForm";
+import Router from "next/router";
+import { useSelector } from "react-redux";
+
+const Profile = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      Router.replace("/");
+    }
+  }, [isLoggedIn]);
+
+  const followerList = [
+    { nickname: "User A" },
+    { nickname: "User B" },
+    { nickname: "User C" },
+  ];
+
+  const followingList = [
+    { nickname: "User 1" },
+    { nickname: "User 2" },
+    { nickname: "User 3" },
+  ];
+
+  return (
+    <AppLayout>
+      <Head>
+        <title>NodeBird</title>
+      </Head>
+      <NicknameEditForm />
+      <FollowList header="Following List" data={followingList} />
+      <FollowList header="Follower List" data={followerList} />
+    </AppLayout>
+  );
+};
 
 export default Profile;
